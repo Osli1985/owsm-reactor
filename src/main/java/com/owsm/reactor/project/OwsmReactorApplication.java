b16ace9dd5fb56debc6b011204943a70f077a7f4
omar slimani <omar.slimani-ext@natixis.com>
2022-05-02 21:23:28 +0200

    init Project

---
+++ src/main/java/com/owsm/reactor/project/OwsmReactorApplication.java
@@ -0,0 +1,13 @@
+package com.owsm.reactor.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwsmReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwsmReactorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/owsm/reactor/project/OwsmReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owsm.reactor.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwsmReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
